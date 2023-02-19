7d568a8575b259f4b109f7fd429548021a1da004
Mr-kelvin-112 <chienyimax64@gmail.com>
2023-02-19 14:59:51 -0800

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/*
+ * main - using for loop to print digits
+ *
+ * Return: 0 always(success)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		printf("%d", num);
+	}
+	printf("\n");
+return (0);
+}
