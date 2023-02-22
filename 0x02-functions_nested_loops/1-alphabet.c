7466bdf8205e1853166b99d11f1dc21ac106d2dc
Mr-kelvin-112 <chienyimax64@gmail.com>
2023-02-22 14:23:46 -0800

    C is getting fun

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - using _putchar to print alphabet 
+ *
+ * Return: 0 always as success
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for(i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+return;
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,5 +2,6 @@
 #define OKEY_FINE
 
 int _putchar(char);
+void print_alphabet(void);
 
 #endif
