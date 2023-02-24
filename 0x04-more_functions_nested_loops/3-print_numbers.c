555bb0b2964303c67ba8c2052ec1acc3b397c7cd
Mr-kelvin-112 <chienyimax64@gmail.com>
2023-02-24 05:57:34 -0800

    The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - function for printing numbers
+ *
+ * Return: 0 always success
+ */
+void print_numbers(void)
+{
+	int num;
+
+	for (num = 48; num < 58; num++)
+	{
+	_putchar(num);
+	}
+	_putchar('\n');
+}
